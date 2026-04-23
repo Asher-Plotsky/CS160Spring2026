6aa88d096a2ce4ad0c310a9ef7c62473f405bab3
Asher Plotsky <asher.plotsky@gmail.com>
2026-04-22 17:35:35 -0700

    Program 7

---
+++ src/Project7/CustomLinkedList.java
@@ -0,0 +1,4 @@
+package Project7;
+
+public class CustomLinkedList {
+}
---
+++ src/Project7/RecursiveMethods.java
@@ -0,0 +1,10 @@
+package Project7;
+/*
+Program 6 Queue Application
+Use a queue to simulate a drive through line.
+CS160-03
+4/16/2026
+@author Asher Plotsky
+*/
+public class RecursiveMethods {
+}
